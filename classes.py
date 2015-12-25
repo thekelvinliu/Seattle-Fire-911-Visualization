c99a7fbfbb1de17452dc80e0d6c1070a3879983b
thekelvinliu <kelvin@thekelvinliu.com>
2015-12-25 15:02:31 -0600

    new logic

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from threading import Thread
 import time
+from pytz import timezone
 import requests
 
 class ApiPoller(Thread):
@@ -10,14 +11,15 @@ class ApiPoller(Thread):
     provided by data.seattle.gov.
     """
     def __init__(self, url, params=None, init_load=24, interval=1,
-                 dt_field='datetime', uid_field=None):
+                 dt_field='datetime', tz='US/Pacific', uid_field=None):
         """Initializes an ApiPoller object.
 
         Given an API endpoint, `url`, this object will load data from the most
         recent `init_load` hours into a container. After, it will continuously
         check the API every `interval` minutes for new data.
-        The data returned from the API must have a field related to date and/or
-        time. This is specified in `dt_field`.
+        The data returned from the API must have a field related to datetime.
+        This field should be specified in `dt_field`. Additionally, the timezone
+        of the datetime should be specified in `tz`.
         Individual piece of data are differentiated by the field specified in
         `uid_field`. If no field is given, then `dt_field` is used.
 
@@ -28,8 +30,10 @@ class ApiPoller(Thread):
                 (int, default 24)
             interval: The number of minutes to wait between polling (int,
                 default 1)
-            dt_field: The name of the field hoding date and/or time information
+            dt_field: The name of the field containing datetime information
                 (string, default 'datetime')
+            tz: The timezone of the datetime information (string, default
+                'US/Pacific')
             uid_field: The name of the field which uniquely identifies data
             (string, default None)
         """
@@ -39,26 +43,28 @@ class ApiPoller(Thread):
         self.init_load = init_load
         self.interval = interval
         self.dt_field = dt_field
+        self.tz = timezone(tz)
         self.uid_field = uid_field
         # INITIATE FIRST REQUEST
-        #save current unix timestamp
-        self.updated = int(time.time())
+        #save current time
+        self.updated = dt.datetime.now(self.tz)
         #send request
         r = requests.get(self.url, params=self.create_payload(initial=True))
-        print(r.url)
+        print r.url
         #save response
         self.data = r.json()
         #add identifiers
         self.uids = set()
+        #use given uid field
         if self.uid_field is not None:
             for d in self.data:
                 self.uids.add(d[self.uid_field])
+        #or use unix timestamp of self.dt_field
         else:
             for d in self.data:
                 uid = dt.datetime.strptime(d[self.dt_field],
                                            "%Y-%m-%dT%H:%M:%S.%f")
                 self.uids.add(int(uid.timestamp()))
-        print(self.uids)
         # THREADING
         super(ApiPoller, self).__init__()
         self.daemon = True
@@ -78,40 +84,50 @@ class ApiPoller(Thread):
         ret = self.params if self.params is not None else dict()
         #start at self.init_load hours before right now
         if initial:
-            start_dt = dt.datetime.now() - dt.timedelta(hours=self.init_load)
+            start_dt = self.updated - dt.timedelta(hours=self.init_load)
         #start at self.updated
         else:
-            start_dt = dt.datetime.fromtimestamp(self.updated)
+            start_dt = self.updated
         #convert to string
         dt_string = start_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")
         #add constriants
         ret["$where"] = "{} >= '{}'".format(self.dt_field, dt_string)
-        ret["$order"] = "{} DESC".format(self.dt_field)
+        ret["$order"] = "{} ASC".format(self.dt_field)
         return ret
 
     def run(self):
-        """Continuously checks the API for new data"""
+        """Continuously checks the API for new data."""
         while self.keep_going:
             #sleep first
             time.sleep(60*self.interval)
-            #then update
-            print("updating...")
-            tmp_time = int(time.time())
+            #save current time
+            tmp_time = dt.datetime.now(self.tz)
             #send request
             r = requests.get(self.url, params=self.create_payload())
+            print r.url
             #check for new data
+            tmp_data = r.json()
+            #go to next iteration is no data is returned
+            if not tmp_data:
+                print "nothing to add"
+                continue
+            #otherwise, there must be new data
             if self.uid_field is not None:
-                for d in r.json():
+                for d in tmp_data:
                     if d[self.uid_field] not in self.uids:
+                        print "adding uid", d[self.uid_field]
                         self.uids.add(d[self.uid_field])
+                        self.data.append(d)
             else:
-                for d in self.data:
+                for d in tmp_data:
                     uid = dt.datetime.strptime(d[self.dt_field],
                                                "%Y-%m-%dT%H:%M:%S.%f")
                     uid = int(uid.timestamp())
                     if uid not in self.uids:
+                        print "adding uid", uid
                         self.uids.add(uid)
-            #save current unix timestamp
+                        self.data.append(d)
+            #adjust self.updated to reflect new time
             self.updated = tmp_time
 
     def stop(self):
--- quick_test.py
+++ quick_test.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
+from __future__ import print_function
 import time
 from classes import ApiPoller
 
 def main():
     ap = ApiPoller("https://data.seattle.gov/resource/grwu-wqtk.json",
                    uid_field="incident_number")
-    #sleep 5 mins
-    time.sleep(5*60)
+    #sleep 20 seconds
+    for i in range(20):
+        print(i, ap.updated)
+        time.sleep(1)
+    #sleep 30 mins
+    time.sleep(30*60)
     print("stopping ap")
     ap.stop()
     ap.join()
