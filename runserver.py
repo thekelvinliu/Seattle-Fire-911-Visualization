46733938230b04119c718bf55ca49d2d0cc1ae10
thekelvinliu <kelvin@thekelvinliu.com>
2015-12-26 20:21:03 -0600

    no more debug

--- runserver.py
+++ runserver.py
@@ -4,6 +4,5 @@ from os import environ
 from WebApp import app as application
 
 if __name__ == "__main__":
-    application.debug = True
-    # application.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+    application.debug = False
     application.run(port=int(environ.get('PORT', 5000)))
