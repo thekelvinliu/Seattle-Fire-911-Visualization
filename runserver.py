89aa6ad9379d17c6d045b9be1d30b9b89cfa20b5
thekelvinliu <kelvin@thekelvinliu.com>
2016-01-18 22:42:17 -0600

    no more debug yay

--- runserver.py
+++ runserver.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-from os import environ
 from WebApp import app as application
 
 if __name__ == "__main__":
-    application.debug = True
-    application.run(port=int(environ.get('PORT', 5000)))
+    application.run()
